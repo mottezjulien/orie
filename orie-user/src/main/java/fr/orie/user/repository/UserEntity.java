a0233b404dcb8db02f8e91fad9cecb19e9bff8d7
Julien <mottez.julien@gmail.com>
2018-04-12 15:35:42 +0200

    - debug user module

--- orie-user/src/main/java/fr/orie/user/repository/UserEntity.java
+++ orie-user/src/main/java/fr/orie/user/repository/UserEntity.java
@@ -1,17 +1,29 @@
 package fr.orie.user.repository;
 
-import fr.jmottez.parc.generic.entity.AbstractEntityUuId;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "USER")
-public class UserEntity extends AbstractEntityUuId {
+public class UserEntity {
+
+    @Column(name="UUID")
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String uuId;
 
     @Column(name = "LOGIN")
     private String login;
+
+    public String getUuId() {
+        return uuId;
+    }
+
+    public void setUuId(String uuId) {
+        this.uuId = uuId;
+    }
 
     public String getLogin() {
         return login;
