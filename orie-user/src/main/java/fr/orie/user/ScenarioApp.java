f3a04d06def49a42d0162c059b7008849d483b8d
Julien <mottez.julien@gmail.com>
2018-04-12 15:37:10 +0200

    - add sprint in user module

---
+++ orie-user/src/main/java/fr/orie/user/ScenarioApp.java
@@ -0,0 +1,12 @@
+package fr.orie.user;
+
+import org.springframework.boot.SpringApplication;
+
+public class ScenarioApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScenarioSpringConfiguration.class);
+    }
+
+
+}
